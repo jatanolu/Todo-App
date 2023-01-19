9b7b0f20c56a39de7eee5d69e73009a3fec60982
noahjean <noahtjean@gmail.com>
2023-01-19 12:55:53 -0600

    making it look good

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 function addItem(){
+    let Div = document.createElement('div')
+    Div.id = 'tasks'
+    document.getElementById('container').append(Div)
     let Item = document.createElement("h2")
-    Item.setAttribute["id", "newitem"]
+    Item.id = 'newitem'
     let itemname = document.getElementById('myinput').value
     console.log(document.getElementById('myinput').value)
     Item.textContent = itemname
-    document.getElementById('tasks').append(Item)
+    Div.appendChild(Item)
+    let delButton = document.createElement('button')
+    delButton.id = 'delItem'
+    delButton.className = 'button'
+    Div.appendChild(delButton)
+    delButton.onclick = deleter
+    Div.onclick = checker
+}
+
+function deleter() {
+    Item = this.parentElement
+    Item.remove()
+}
+
+function checker(){
+    Div = this
+    if (Div.style.backgroundColor === 'green'){
+        Div.style.backgroundColor =  'rgba(128, 0, 0, .5)'
+    }
+    else{
+        Div.style.backgroundColor = 'green'
+    }
 }
